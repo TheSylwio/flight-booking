f7acda6bba62e15def64eaf5b981936d85451502
TheSylwio <thesylwio@gmail.com>
2022-01-05 00:55:17 +0100

    Add relation and fields

--- src/Entity/Reservation.php
+++ src/Entity/Reservation.php
@@ -5,14 +5,21 @@ namespace App\Entity;
 use DateTimeInterface;
 
 class Reservation {
+	public const STATUS_VALID = 'VALID';
+	public const STATUS_CANCELED = 'CANCELED';
+	public const STATUS_EXPIRED = 'EXPIRED';
+
 	private ?int $id;
 	private ?string $user;
-	private ?Flight $flight;
+	private ?string $status;
+	private Flight $flight;
+	private Seat $seat;
 	private ?DateTimeInterface $datetime;
 
 	public function getId(): ?int {
 		return $this->id;
 	}
+
 
 	public function getUser(): ?string {
 		return $this->user;
@@ -21,13 +28,29 @@ class Reservation {
 	public function setUser(string $user): void {
 		$this->user = $user;
 	}
-	
+
+	public function getStatus(): ?string {
+		return $this->status;
+	}
+
+	public function setStatus(?string $status): void {
+		$this->status = $status;
+	}
+
 	public function getFlight(): ?Flight {
 		return $this->flight;
 	}
 
 	public function setFlight(?Flight $flight): void {
 		$this->flight = $flight;
+	}
+
+	public function getSeat(): Seat {
+		return $this->seat;
+	}
+
+	public function setSeat(Seat $seat): void {
+		$this->seat = $seat;
 	}
 
 	public function getDatetime(): ?DateTimeInterface {
