30612e08537a077aece43c54cf25178b11c60bd1
TheSylwio <thesylwio@gmail.com>
2022-01-07 14:23:06 +0100

    Handle creating seat on reservation

--- src/Controller/ReservationController.php
+++ src/Controller/ReservationController.php
@@ -3,8 +3,10 @@
 namespace App\Controller;
 
 use App\Entity\Reservation;
+use App\Entity\Seat;
 use App\Repository\FlightRepository;
 use App\Repository\SeatRepository;
+use DateTime;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -15,25 +17,37 @@ class ReservationController extends AbstractController {
 	public function create(Request $request, SeatRepository $seatRepository, FlightRepository $flightRepository, EntityManagerInterface $em): JsonResponse {
 		$data = json_decode($request->getContent());
 
+		$flight = $flightRepository->find($data->flight);
+		$plane = $flight->getPlane();
+
+		$chosenSeat = $seatRepository->findBy(['orderNumber' => $data->seat, 'plane' => $plane]);
+		if ($chosenSeat) {
+			return $this->json(sprintf('Seat with order number: %d already exists!', $data->seat), Response::HTTP_BAD_REQUEST);
+		}
+
+		$seat = new Seat();
+		$seat->setPlane($plane);
+		$seat->setOrderNumber($data->seat);
+
 		$reservation = new Reservation();
-		$reservation->setDatetime(new \DateTime());
+		$reservation->setDatetime(new DateTime());
 		$reservation->setStatus($data->status);
-		$reservation->setSeat($seatRepository->find($data->seat));
-		$reservation->setFlight($flightRepository->find($data->flight));
+		$reservation->setSeat($seat);
+		$reservation->setFlight($flight);
 		$reservation->setUser('testUser'); // FIXME
 
+		$em->persist($seat);
 		$em->persist($reservation);
 		$em->flush();
 
 		return $this->json('Entity created', Response::HTTP_CREATED);
 	}
 
-	public function patch(Request $request,  EntityManagerInterface $em): JsonResponse {
+	public function patch(Request $request, EntityManagerInterface $em): JsonResponse {
 		$data = json_decode($request->getContent());
 
-		$res = $em->getRepository(Reservation::class)->find($data->id);
-
-		$res->setStatus($data->status);
+		$reservation = $em->getRepository(Reservation::class)->find($data->id);
+		$reservation->setStatus($data->status);
 
 		$em->flush();
 
